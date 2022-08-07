6bbabef1c61ae441069aacdf6d7b3fe6a321d038
Solomon, David <david.solomon@dell.com>
2022-08-07 12:09:54 -0400

    create-react app

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href ="/">Medicine</a>
+      </header>
+      <main>List of medicine</main>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/data.js
@@ -0,0 +1,54 @@
+const data = {
+    medicine: [
+        {
+            name:'Advil',
+            slug:'Advil-Liqui-Gels',
+            category:'Pain Management',
+            image: '/images/p1.jpg',  //679px x 829px
+            price: 23.01,
+            countInStock: 15,
+            brand: 'Ibuprofen Pain Reliever/Fever Reducer (NSAID)',
+            rating: 4.9,
+            numReviews: 1000,
+            description: 'Advil is the top selling over-the-counter pain reliever in the world'
+        },
+        {
+            name:'Tylenol',
+            slug:'Tylenol-Extra-Strength',
+            category:'Pain Management',
+            image: '/images/p2.jpg',
+            price: 17.49,
+            countInStock: 10,
+            brand: 'Acetaminophen Pain Reliever and Fever Reducer',
+            rating: 4.7,
+            numReviews: 1000,
+            description: 'Provides temporary relief of minor aches and pains and helps reduce fever'
+        },
+        {
+            name:'Amoxicillian',
+            slug:'Amoxicillian',
+            category:'Antibiotic',
+            image: '/images/p3.jpg',
+            price: 38.99,
+            countInStock: 15,
+            brand: 'Used to treat bacterial infections',
+            rating: 4.6,
+            numReviews: 1000,
+            description: 'Amoxicillin is a penicillin antibiotic. It is used to treat bacterial infections, such as chest infections (including pneumonia) and dental abscesses.'
+        },
+        {
+            name:'Spironolactone',
+            slug:'Spironolactone',
+            category:'High Blood Pressure',
+            image: '/images/p4.jpg',
+            price: 35.69,
+            countInStock: 5,
+            brand: 'Spironolactone for High Blood Pressure',
+            rating: 4.3,
+            numReviews: 1000,
+            description: 'Used to treat high blood pressure and hear failiure'
+        },
+    ],
+};
+
+export default data;
